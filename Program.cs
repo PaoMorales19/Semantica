811123e77dfb61515ebcd01f17fe3919d9414985
PaoMorales19 <ap.morales.anaya@gmail.com>
2022-09-19 09:37:15 -0500

    Primera actualizacion

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace Semantica
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Lenguaje a = new Lenguaje();//C:\\Users\\HOME\\Documents\\PAOLA TAREAS\\5TO SEMESTRE\\AUTOMATAS II\\UNIDAD 1\\Evalua\\prueba.cpp"
+
+                a.Programa();
+                /*while(!a.FinArchivo())
+                {
+                    a.NextToken();
+                }*/
+                a.cerrar();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
